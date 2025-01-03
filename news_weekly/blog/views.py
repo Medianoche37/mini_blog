a058b12a2739cdc561f2550a6604d4b0112aea94
Nastasya <nastena_rozova@mail.ru>
2025-01-03 20:37:23 +0300

    Final

--- news_weekly/blog/views.py
+++ news_weekly/blog/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render, redirect
 from django.views.generic.base import View
-from .models import Post
+from .models import Post, Likes
 from .form import CommentsForm
+
 
 class PostView(View):
     '''вывод записей'''
+
     def get(self, request):
         posts = Post.objects.all()
         return render(request, 'blog/blog.html', {'post_list': posts})
@@ -12,12 +14,15 @@ class PostView(View):
 
 class PostDetail(View):
     '''отдельная страница записи'''
+
     def get(self, request, pk):
         post = Post.objects.get(id=pk)
         return render(request, 'blog/blog_detail.html', {'post': post})
 
+
 class AddComments(View):
     '''добавление комментариев'''
+
     def post(self, request, pk):
         form = CommentsForm(request.POST)
         if form.is_valid():
@@ -26,4 +31,35 @@ class AddComments(View):
             form.save()
         return redirect(f'/{pk}')
 
-# Create your views here.
+
+def get_client_ip(request):
+    x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
+    if x_forwarded_for:
+        ip = x_forwarded_for.split(',')[0]
+    else:
+        ip = request.META.get('REMOTE_ADDR')
+    return ip
+
+class AddLike(View):
+    def get(self,request,pk):
+        ip_client = get_client_ip(request)
+        try:
+            Likes.objects.get(ip=ip_client, pos_id=pk)
+            return redirect(f'/{pk}')
+        except:
+            new_like = Likes()
+            new_like.ip = ip_client
+            new_like.pos_id = int(pk)
+            new_like.save()
+            return redirect(f'/{pk}')
+
+
+class DelLike(View):
+    def get(self, request, pk):
+        ip_client = get_client_ip(request)
+        try:
+            lik = Likes.objects.get(ip=ip_client, pos_id=pk)
+            lik.delete()
+            return redirect(f'/{pk}')
+        except:
+            return redirect(f'/{pk}')
